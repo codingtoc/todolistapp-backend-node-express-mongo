6bd1fa96f74aa9e3cd3a05ecc52e829ff6c08b0a
codingtoc <codingtoc@gmail.com>
2018-12-05 00:57:22 +0900

    MongoDB에 연결하기(index.js 변경)

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const express = require("express");
 // 라우트 모듈 가져오기
 const routes = require("./routes/api");
+// mongoose 모듈 가져오기
+const mongoose = require("mongoose");
 
 // express app 생성하기
 const app = express();
+
+// MongoDB에 연결하기
+mongoose.connect(
+  "mongodb://localhost:27017/todolist",
+  { useNewUrlParser: true }
+);
+// mongoose의 기본 Promise(mPromise)를 node의 Promise로 교체
+mongoose.Promise = global.Promise;
 
 // application/json 타입의 body를 파싱하기 위한 미들웨어 등록
 app.use(express.json());
