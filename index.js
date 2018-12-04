6aef4e2b84f70fc01671e02f1ca8622f95e318ff
codingtoc <codingtoc@gmail.com>
2018-12-04 21:35:40 +0900

    라우트 초기화(index.js 변경)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // express 모듈 가져오기
 const express = require("express");
+// 라우트 모듈 가져오기
+const routes = require("./routes/api");
 
 // express app 생성하기
 const app = express();
+
+// 라우트 초기화
+app.use("/api", routes);
 
 // 4000번 포트에서 요청 리스닝 하기
 app.listen(4000, () => {
