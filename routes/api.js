a947f72222fcc65dace0b9384b5f7c74754355a1
codingtoc <codingtoc@gmail.com>
2018-12-05 01:41:32 +0900

    에러 핸들링(routes/api.js 변경)

--- routes/api.js
+++ routes/api.js
@@ -6,22 +6,24 @@ const router = express.Router();
 const Todo = require("../models/todo");
 
 // DB에서 할일 목록 가져오기
-router.get("/todos", (req, res) => {
+router.get("/todos", (req, res, next) => {
   res.send({ type: "GET" });
 });
 // DB에 신규 할일 추가하기
-router.post("/todos", (req, res) => {
+router.post("/todos", (req, res, next) => {
   // MongoDB에 신규 할일 추가하기
-  Todo.create(req.body).then(todo => {
-    res.send(todo);
-  });
+  Todo.create(req.body)
+    .then(todo => {
+      res.send(todo);
+    })
+    .catch(next);
 });
 // DB에서 기존 할일 수정하기
-router.put("/todos/:id", (req, res) => {
+router.put("/todos/:id", (req, res, next) => {
   res.send({ type: "PUT" });
 });
 // DB에서 기존 할일 삭제하기
-router.delete("/todos/:id", (req, res) => {
+router.delete("/todos/:id", (req, res, next) => {
   res.send({ type: "DELETE" });
 });
 
